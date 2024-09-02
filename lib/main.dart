57ae96ee3ae1fbed98f81bdc3a57707224010a95
Dinath01 <dinithnethmina101@gmail.com>
2024-09-02 17:16:17 +0530

    FireBase authenticated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:text_app/pages/login_page.dart';
-import 'package:text_app/pages/register_page.dart';
-import 'package:text_app/services/auth/login_or_register.dart';
+import 'package:text_app/firebase_options.dart';
+import 'package:text_app/services/auth/auth_gate.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
